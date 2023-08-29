4b41b48b7de7bd407214efdb3ce8221b6c104373
Serg <seleznevs79@mail.ru>
2023-08-29 21:26:32 +0700

    1_1п

--- Program.cs
+++ Program.cs
@@ -8,37 +8,37 @@
 
 // 8 7,8 -7,1 9
 
-// Console.WriteLine("Введите количество строк массива: ");
-// int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
 
-// Console.WriteLine("Введите количество столбцов массива: ");
-// int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
 
-// double[,] array = GetArray(rows, columns, 0, 10);
-// PrintArray(array);
+double[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
 
 
 
-// // Создание массива
-// double[,] GetArray(int m, int n, double minValue, double maxValue)
-// {
-// double[,] res = new double[m,n];
-// for(int i = 0; i < m; i++){
-//  for(int j = 0; j < n; j++){
-//    res [i,j] =Random().NextDouble(minValue, maxValue + 1);
-//  }
-// }
-// return res;
-// }
-// // Печать массива
-// void PrintArray(double[,] array){
-// for(int i = 0; i < array.GetLength(0); i++){
-// for(int j = 0; j < array.GetLength(1); j++){
-// Console.Write($"{array[i,j]} ");
-// }
-// Console.WriteLine();
-// }
-// }
+// Создание массива
+double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+double[,] res = new double[m,n];
+for(int i = 0; i < m; i++){
+ for(int j = 0; j < n; j++){
+   res [i,j] =new Random().NextDouble()*10;
+ }
+}
+return res;
+}
+// Печать массива
+void PrintArray(double[,] array){
+for(int i = 0; i < array.GetLength(0); i++){
+for(int j = 0; j < array.GetLength(1); j++){
+Console.Write($" ; {array[i,j]}");
+}
+Console.WriteLine();
+}
+}
 
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и 
@@ -111,42 +111,42 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
-Console.WriteLine("Введите количество строк массива: ");
-int rows = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введите количество столбцов массива: ");
-int columns = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine()!);
 
-int [,] array = GetArray(rows, columns, 0, 9);
-PrintArray(array);
+// int [,] array = GetArray(rows, columns, 0, 9);
+// PrintArray(array);
 
 
-// Создание массива
-int[,] GetArray(int m, int n, int minValue, int maxValue)
-{
-int[,] res = new int[m,n];
-for(int i = 0; i < m; i++){
-for(int j = 0; j < n; j++){
-res[i,j] = new Random().Next(minValue, maxValue + 1);
-}
-}
-return res;
-}
-// Печать массива
-void PrintArray(int[,] array){
-for(int i = 0; i < array.GetLength(0); i++){
-for(int j = 0; j < array.GetLength(1); j++){
-Console.Write($"{array[i,j]} ");
-}
-Console.WriteLine();
-}
-}
+// // Создание массива
+// int[,] GetArray(int m, int n, int minValue, int maxValue)
+// {
+// int[,] res = new int[m,n];
+// for(int i = 0; i < m; i++){
+// for(int j = 0; j < n; j++){
+// res[i,j] = new Random().Next(minValue, maxValue + 1);
+// }
+// }
+// return res;
+// }
+// // Печать массива
+// void PrintArray(int[,] array){
+// for(int i = 0; i < array.GetLength(0); i++){
+// for(int j = 0; j < array.GetLength(1); j++){
+// Console.Write($"{array[i,j]} ");
+// }
+// Console.WriteLine();
+// }
+// }
 
-for(int j = 0; j < array.GetLength(1); j++){
-   double sum = 0;  
-for(int i = 0; i < array.GetLength(0); i++){
-    sum += array [i,j];
-}
-double sred=sum/ array.GetLength(1);
-Console.Write($" ; { sred} ");
-}
+// for(int j = 0; j < array.GetLength(1); j++){
+//    double sum = 0;  
+// for(int i = 0; i < array.GetLength(0); i++){
+//     sum += array [i,j];
+// }
+// double sred=sum/ array.GetLength(1);
+// Console.Write($" ; { sred} ");
+// }
